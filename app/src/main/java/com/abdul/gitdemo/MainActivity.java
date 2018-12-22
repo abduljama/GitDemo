ab02c8f349d71ca8a9a8f94c40fa0f9041811c76
abduljama <abduljama829@gmail.com>
2018-12-22 23:00:01 +0300

    THis is a test.

--- app/src/main/java/com/abdul/gitdemo/MainActivity.java
+++ app/src/main/java/com/abdul/gitdemo/MainActivity.java
@@ -2,12 +2,15 @@ package com.abdul.gitdemo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.RelativeLayout;
 
 public class MainActivity extends AppCompatActivity {
+    RelativeLayout mytestlayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
