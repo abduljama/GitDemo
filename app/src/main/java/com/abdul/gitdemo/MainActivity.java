60720393080d2970ca186afe8506eab86fee8ad8
abduljama <abduljama829@gmail.com>
2018-12-22 23:06:11 +0300

    `This is test on a different branch `

--- app/src/main/java/com/abdul/gitdemo/MainActivity.java
+++ app/src/main/java/com/abdul/gitdemo/MainActivity.java
@@ -4,11 +4,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     RelativeLayout mytestlayout;
     Button mybutton;
-
+    TextView txt;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
