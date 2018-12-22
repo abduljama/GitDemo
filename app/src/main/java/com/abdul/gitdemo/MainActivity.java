d793f2f41b4749da26d0c85cf9f93172c512ff59
abduljama <abduljama829@gmail.com>
2018-12-22 23:02:39 +0300

    `This is test on a different branch `

--- app/src/main/java/com/abdul/gitdemo/MainActivity.java
+++ app/src/main/java/com/abdul/gitdemo/MainActivity.java
@@ -2,15 +2,19 @@ package com.abdul.gitdemo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.RelativeLayout;
 
 public class MainActivity extends AppCompatActivity {
     RelativeLayout mytestlayout;
+    Button mybutton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+       // mybutton = new Button();
+
 
     }
 }
